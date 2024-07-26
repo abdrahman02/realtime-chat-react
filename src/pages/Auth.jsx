38af8a173cdec020b8bb9cc87d76eb96650f08f0
abdrahman02 <mabdulr44@gmail.com>
2024-07-26 13:19:30 +0700

    responsive auth form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -5,8 +5,8 @@ import { memo } from "react";
 
 const Auth = ({ formType }) => {
   return (
-    <main className="w-full min-h-[90vh] flex justify-center items-center">
-      <Card className="w-1/3 shadow-md">
+    <main className="w-full min-h-[90vh] px-2 flex justify-center items-center">
+      <Card className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 shadow-md">
         <AuthLayout formType={formType} />
       </Card>
     </main>
